9aa4ed311866535592449ebed2a28c3d8fb428b3
Ronny Rios <ronnie.rios@fullstackacademy.com>
2023-05-26 17:15:01 -0700

    initial files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Task # 1
+- given an array of food items, display all of them when i click a button
+
+# Task # 2
+- make a function that shows only the fruits or veggies
---
+++ script.js
@@ -0,0 +1,11 @@
+const list = [
+    { id: 1, name: 'carrots' },
+    { id: 2, name: 'oranges' },
+    { id: 3, name: 'broccoli' },
+    { id: 4, name: 'bananas' },
+    { id: 5, name: 'onions' },
+    { id: 6, name: 'melons' },
+    { id: 7, name: 'lettuce' },
+]
+
+//write code here
